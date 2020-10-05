5f2fad29a426412bab67a4ede3c8c56a59e9f0ef
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-10-05 07:51:38 +0200

    physics based character controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _gravity = 1.0f;
+    [SerializeField]
+    private float _jumpHeight = 30.0f;
+    private float _yVelocity;
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * _speed;
+
+        if (_controller.isGrounded == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+            }
+        }
+        else
+        {
+            _yVelocity -= _gravity;
+        }
+
+        velocity.y = _yVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
